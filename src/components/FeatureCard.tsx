635db45a76253213695059db16a87c4fed551e61
Robiquet <tom-robiquet@hotmail.co.uk>
2021-05-22 14:20:20 +0100

    Home card styling

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,17 +1,9 @@
-import styled from "styled-components";
 
-const Image = styled.div`
-  width: 250px;
-  height: 250px;
-  background-color: grey;
-  border: 1px solid black;
-`;
-
-const FeatureCard = (props: {children:any}) => {
+const FeatureCard = (props: {title:string, description:string}) => {
   return (
-    <div className="flex flex-col items-center">
-      <Image></Image>
-      <p className="font-normal text-base">{props.children}</p>
+    <div className="flex flex-col items-center justify-between	max-w-xs max-h-56 w-full h-full rounded bg-green-200 p-5 ">
+      <h3 className="text-4xl font-extrabold mb-6">{props.title}</h3>
+      <p className="font-normal text-base text-center	">{props.description}</p>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,27 @@ import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 import { useState } from "react";
 import FeatureCard from "../components/FeatureCard";
 
-
 const Home = () => {
   const [balance, setBalance] = useState();
   const [publicKey, setPublicKey] = useState();
+
+  const features = [
+    {
+      title: "Liquidity",
+      description:
+        "Get an overview of APR on all the liquidity pools. Including historical average and future projections.",
+    },
+    {
+      title: "Stake",
+      description:
+        "Stake your assets in one click and never let your assets sit idle in your account. No one likes freeloaders.",
+    },
+    {
+      title: "Automate",
+      description:
+        "Onchain strategies to automate your trading. Let Bots do the trading.",
+    },
+  ];
 
   const handleConnect = async () => {
     const connection = new Connection(clusterApiUrl("devnet"));
@@ -28,14 +45,21 @@ const Home = () => {
 
   return (
     <div className="flex flex-col md:h-screen justify-around items-center flex-grow">
-      <h1 className="font-bold text-2xl">DeFi Investing shouldn't be Rocket Science</h1>
-      <h2 className="font-semibold text-lg">DeFi is complicated, we make it simple</h2>
-      <div className="flex flex-wrap justify-evenly w-screen">
-        <FeatureCard>Get an overview of rates</FeatureCard>
-        <FeatureCard>Stake your assets in one click</FeatureCard>
-        <FeatureCard>Automate your trading with bots</FeatureCard>
+      <h1 className="font-bold text-2xl">
+        DeFi Investing shouldn't be Rocket Science
+      </h1>
+      <h2 className="font-semibold text-lg">
+        DeFi is complicated, we make it simple
+      </h2>
+      <div className="flex flex-wrap justify-evenly w-full">
+        {features.map(feature => <FeatureCard title={feature.title} description={feature.description}></FeatureCard>)}
       </div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-52" onClick={handleConnect}>Connect Wallet</button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-52"
+        onClick={handleConnect}
+      >
+        Connect Wallet
+      </button>
       <div>{balance}</div>
       <div>{publicKey}</div>
     </div>
