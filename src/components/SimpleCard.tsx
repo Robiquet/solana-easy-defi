61944ce4682678188800080c2ec77b4b3044ced2
Robiquet <tom-robiquet@hotmail.co.uk>
2021-05-31 12:18:09 +0100

    Created simple card

---
+++ src/components/SimpleCard.tsx
@@ -0,0 +1,14 @@
+export interface SimpleCardConfig {
+  header: string;
+  info: string;
+}
+
+const SimpleCard = ({ details }: { details: SimpleCardConfig }) => {
+  return (
+    <div className="flex flex-col rounded bg-green-200 p-5 w-80 cursor-pointer">
+      <h4 className="text-4xl font-bold mb-6">Header</h4>
+      <div className="text-6xl font-black">Content</div>
+    </div>
+  );
+};
+export default SimpleCard;
